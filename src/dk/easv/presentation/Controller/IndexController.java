6272bd513fd87bc222ee4ecfba02291044b34825
TheDarkSide1992 <Jenscp171@gmail.com>
2023-02-03 11:35:09 +0100

    FXML Load FX File in file

--- src/dk/easv/presentation/Controller/IndexController.java
+++ src/dk/easv/presentation/Controller/IndexController.java
@@ -3,10 +3,12 @@ package dk.easv.presentation.Controller;
 import dk.easv.logic.LogicManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,8 +20,26 @@ public class IndexController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         logicManager = new LogicManager();
-        logicManager.openNewView("LogIn.fxml", "Movie recommendation system");
-        if (false) System.exit(0); //TODO if login is not given, close program
+
+        try {
+            loadLogIn();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void loadLogIn() throws IOException {
+        //"src/dk/easv/presentation/Controller/View/LogIn.fxml"
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/dk/easv/presentation/View/LogIn.fxml"));
+        BorderPane newScene = loader.load();
+
+        borderPane.setCenter(newScene);
+
+
+        /*logicManager.openNewView("LogIn.fxml", "Movie recommendation system");
+        if (false) System.exit(0); //TODO if login is not given, close program */
     }
 
     public void handleMenu(ActionEvent event) {
